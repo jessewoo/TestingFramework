7a58a3fc7ce01688ebfe895f4a6b38b19857725c
Jesse Woo <woojesse@gmail.com>
2016-09-07 18:12:23 -0700

    use of page-object gem

--- test_puppies/features/support/pages/shopping_cart_page.rb
+++ test_puppies/features/support/pages/shopping_cart_page.rb
@@ -1,13 +1,21 @@
 class ShoppingCartPage
   
+  include PageObject
+  
   # Constants
   NAME_COLUMN = 1
   SUBTOTAL_COLUMN = 3
   LINES_PER_PUPPY = 6
   
-  def initialize(browser) 
-    @browser = browser
-  end 
+  button(:proceeed_to_checkout, :value => "Complete the Adoption")
+  button(:continue_adopting, :value => "Adopt Another Puppy")
+  table(:cart, :index => 0)
+  cell(:cart_total, :class => "total_cell")
+  
+  
+  # def initialize(browser) 
+  #     @browser = browser
+  #   end 
 
 
 # General methods called from step definitions
@@ -19,18 +27,17 @@ class ShoppingCartPage
     cart_line_item(line_item)[SUBTOTAL_COLUMN].text
   end
   
-  def cart_total
-    @browser.td(:class => 'total_cell').text
-  end
-  
-  
-  def proceed_to_checkout
-    @browser.button(:value => 'Complete the Adoption').click
-  end
-  
-  def continue_adopting
-    @browser.button(:value => 'Adopt Another Puppy').click
-  end
+  # def cart_total
+  #   @browser.td(:class => 'total_cell').text
+  # end
+  # 
+  # def proceed_to_checkout
+  #   @browser.button(:value => 'Complete the Adoption').click
+  # end
+  # 
+  # def continue_adopting
+  #   @browser.button(:value => 'Adopt Another Puppy').click
+  # end
   
 # 2 methods designed to only be called from other methods of the class. "internal" methods stay internal
   private
@@ -40,7 +47,8 @@ class ShoppingCartPage
   end
   
   def cart_line_item(line_item) 
-    @browser.table(:index => 0)[row_for(line_item)]
+    # @browser.table(:index => 0)[row_for(line_item)]
+    cart_element[row_for(line_item)]
   end
 
 end
